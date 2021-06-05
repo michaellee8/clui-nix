130655854271024879cbb20006cb16c7eaa16b50
michaellee8 <ckmichael8@gmail.com>
2021-06-05 05:26:40 +0000

    feat: implement clui core interfaces

---
+++ backend/go/pkg/clui/connect.go
@@ -0,0 +1,16 @@
+package clui
+
+import "github.com/pkg/errors"
+
+// Connect takes a Provider and a Consumer, perform the connection logic between
+// them. and then starts the Provider. It returns an error if there are any error
+// in the process, otherwise it returns nil.
+func Connect(p Provider, c Consumer) (err error) {
+	p.SetDir(c.Dir())
+	p.SetInput(c.Input())
+	p.SetOutput(c.Output())
+	p.SetCompOptHandler(c.CompOptHandler())
+
+	return errors.Wrap(p.Start(), "clui connect failed")
+
+}
--- backend/go/pkg/clui/consumer.go
+++ backend/go/pkg/clui/consumer.go
@@ -1,4 +1,30 @@
 package clui
 
+import "io"
+
+// Consumer represents any frontend that would like to consume the clui interface
+// for example an Websocket server or a terminal-based UI interface
 type Consumer interface {
+
+	// Dir should return the current working directory that the Consumer intends
+	// to have, it is guaranteed the created process will have such cwd
+	Dir() string
+
+	// Input should return an io.Reader that the Consumer wants the provider to
+	// read input from
+	Input() io.Reader
+
+	// Output should return an io.Writer that the Consumer wants the provider to
+	// write output to
+	Output() io.Writer
+
+	// CompOptHandler should return an CompletionInfoHandler that will receives
+	// all completion information provided by the Provider, it must be safe for
+	// multiple concurrent invocation of Handle()
+	CompOptHandler() CompletionInfoHandler
+
+	// OnStart is a callback that will be called after the Provider has started
+	// the backing process and have all preparation done successfully. It should
+	// only be called once
+	OnStart()
 }
